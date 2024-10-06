bbd307c87a74fa6bcd879a58ca74c8285329cbaf
WangAQ <1098200092@qq.com>
2024-10-06 18:04:38 +0800

    修改了排序算法中的错误

--- main.cpp
+++ main.cpp
@@ -1,33 +1,31 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-void sort( int arr[],int size)
+
+void sort(int arr[], int size)
 {
-    for(int i= 1; i < size; ++i)
+    for (int i = 1; i < size; ++i)
     {
+        int val = arr[i];
         int j = i - 1;
-        int val = arr[j];
-        for(; j>=0&&arr[j]>val ; --j) 
+        for (; j >= 0 && arr[j] > val; --j)
         {
-            arr[j+1] = arr[j];
-
+            arr[j + 1] = arr[j];
         }
-        arr[j+1] = val;
+        arr[j + 1] = val;
     }
-
 }
 
 int main()
 {
-    int arr[]={18,3,78,33,53};
-    int size = sizeof(arr)/sizeof(int);
-    sort(arr,size);
-    for(int& v:arr)
+    int arr[] = {18, 3, 78, 33, 53};
+    int size = sizeof(arr) / sizeof(int);
+    sort(arr, size);
+    for (int &v : arr)
     {
-        cout << v <<" ";
+        cout << v << " ";
     }
     cout << endl;
 
     return 0;
-
 }
