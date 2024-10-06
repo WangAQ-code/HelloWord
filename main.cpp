cdedd51634bebabc3a74f675c1ba68a00e619ecd
WangAQ <1098200092@qq.com>
2024-10-06 17:10:36 +0800

    创建了main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+void sort( int arr[],int size)
+{
+
+}
+
+int main()
+{
+    int arr[]={18,3,78,33,53};
+    int size = sizeof(arr)/sizeof(int);
+    sort(arr,size);
+
+    return 0;
+
+}
