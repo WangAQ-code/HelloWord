73860e28b8f45d796ca5db58ec4432f75021a1fb
windows <w1651162103@163.com>
2024-10-06 17:53:54 +0800

    实现了代码逻辑

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 void sort( int arr[],int size)
 {
+    for(int i= 1; i < size; ++i)
+    {
+        int j = i - 1;
+        int val = arr[j];
+        for(; j>=0&&arr[j]>val ; --j) 
+        {
+            arr[j+1] = arr[j];
+
+        }
+        arr[j+1] = val;
+    }
 
 }
 
@@ -11,6 +22,11 @@ int main()
     int arr[]={18,3,78,33,53};
     int size = sizeof(arr)/sizeof(int);
     sort(arr,size);
+    for(int& v:arr)
+    {
+        cout << v <<" ";
+    }
+    cout << endl;
 
     return 0;
 
